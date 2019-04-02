0019da4b7be25dc7ef06c6efacdebcca9ed7e476
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-04-02 21:37:25 +0430

    modify some files

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ const buildMdg = require("./src/build-mdg");
 const nsga2 = require("./src/nsga-ii");
 const initPopulation = require("./src/init-population");
 const initObjectives = require("./src/init-objectives");
+const io = require("./lib/io");
+const createDot = require("./util/create-dot");
 
 let mdg = {};
 let pop = [];
 let objs = [];
 let fronts = [];
 
+console.log("Reading dataset ...");
 buildMdg("./dataset/nos", (result) => {
+  
   mdg = result;
-  pop = initPopulation(35, mdg.modules.length, 1, 7);
+  pop = initPopulation(70, mdg.modules.length, 1, 7);
   objs = initObjectives(mdg.graph);
-  fronts = nsga2(1000, pop, 35, 7, 0.8, 0.1, objs);
-  console.log(mdg.modules);
-  fronts.forEach(f => {
-    f.forEach(s => {
-      console.log(`
-        Rank = ${s.rank}
-        coh  = ${objs[0].func(s.chromosome)} 
-        cop  = ${objs[1].func(s.chromosome)} 
-        mq   = ${objs[2].func(s.chromosome)} 
-        clusters = [${s.chromosome}]`);
-    })
+
+  console.log("Runing NSGA-II ...");
+  fronts = nsga2(5000, pop, 70, 7, 0.8, 0.1, objs);
+
+  console.log("Creating dot files ...");
+  let dot = createDot(mdg.modules, mdg.graph, fronts[0][0].chromosome);
+  io.createFile("./output/output.dot", dot, (err) => {
+    if (err) console.log(err);
+    console.log("Dot files fineshed ...")
   });
+
+
+
 });
 
-// cn   = ${objs[3].func(s.chromosome)} 
-// dev  = ${objs[4].func(s.chromosome)}       
